25da3b3359481468dc2b1ca1bb7ded2b704c1bb4
tvdtrong <tvdtrong@gmail.com>
2021-07-16 19:45:37 +0700

    add function isStopWord

--- project/utils/tokenizer.cpp
+++ project/utils/tokenizer.cpp
@@ -19,3 +19,25 @@ vector<string> split(string text) {
 
 	return res;
 }
+
+bool isStopWord(string word) {
+    ifstream fin;
+    fin.open("stopword.txt");
+    bool isstopword = false;
+    if (fin.is_open()) {
+        vector<string> stopwords;
+        string get;
+        while (!fin.eof()) {
+            getline(fin, get);
+            stopwords.push_back(get);
+        }
+        for (int i = 0; i < stopwords.size(); i++) {
+            if (word.compare(stopwords[i]) == 0) {
+                isstopword = true;
+                break;
+            }
+        }
+    }
+    fin.close();
+    return isstopword;
+}
