ff4f9eea1b6c741156bf3caf1ae23bd147a9c629
phipk02 <phipk02@gmail.com>
2021-08-09 18:52:29 +0700

    update history

--- project/utils/history.h
+++ project/utils/history.h
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <map>
+#include "utils.h"
 
 class History {
 private:
@@ -13,28 +15,32 @@ private:
 	std::fstream f;
 
 public:
-	std::vector<std::string> getHistory(std::string s) {
-		std::vector<std::string> res;
-		res.push_back(s);
-		
-		int i = 1;
+	std::vector<std::string> getHistory(std::vector <std::string> list) {
 		fin.open("history.txt");
 
+		std::vector<std::string> res;
+
 		if (fin.is_open()) {
-			while (!fin.eof()) {
-				std::string temp; getline(fin, temp);
-				if (temp == "" || temp == s || temp.find(s) == -1) continue;
-
-				std::cout << i++ << ". " << temp << std::endl;
-				res.push_back(temp);
+			std::map<std::string, int> history;
+			while (!fin.eof()) { // To push all word in file history to a map, and set it to 1
+				std::string str; std::getline(fin, str);
+				history[str] = 1;
 			}
+			for (auto s : list)
+				for (auto p : history) { // To see if s is substring of p
+					if (p.first == s || p.first == "" || p.first.find(s) == -1) continue;
+					else p.second++;
+				}
+
+			int i = 1;
+			for (auto p : history)
+				if (p.second > 1) {
+					std::cout << i++ << ". " << p.first << std::endl;
+					res.push_back(p.first);
+				}
 		}
+		else std::cout << "Can't open file history.txt\n", std::system("pause");
 		fin.close();
-
-		fout.open("history.txt", std::ios::app);
-		if(fout.is_open()) fout << s << std::endl;
-		fout.close();
-
 		return res;
 	};
 
@@ -48,17 +54,43 @@ public:
 		std::cout << "0. Search" << std::endl << "1. Show history suggestion" << std::endl << "OPTION:   ";
 		std::cin >> choice; std::cin.ignore();
 		if (choice) {
-			std::vector<std::string> res = getHistory(s);
+			std::vector<std::string> list;
+			tokenize(s, list); // To token the string
+			
+			std::vector<std::string> res = getHistory(list);
+			
+			if (std::find(res.begin(), res.end(), s) != res.end()) { // To check if s existed in file history
+				std::cout << res.size() + 1 << ". " << s << std::endl;
+				res.push_back(s);
+			}
+
 			std::cin >> choice; std::cin.ignore();
 			if (choice > res.size() - 1) {
-				std::cout << "Invalid" << std::endl; choice = 0;
+				std::cout << "Invalid" << std::endl; choice = res.size();
 			}
 			s = res[(std::size_t)choice - 1];
 		}
 		else {
-			fout.open("history.txt", std::ios::app);
-			fout << s << std::endl;
-			fout.close();
+			fin.open("history.txt");
+
+			std::vector<std::string> res;
+			bool isInFile = false;
+
+			if (fin.is_open()) {
+				while (!fin.eof()) { // To check if s existed in file.
+					std::string str; std::getline(fin, str);
+					if (str == s) {
+						isInFile = true;
+						break;
+					}
+				}
+			}
+			fin.close();
+			if (!isInFile) { // If it's in file, update file history.
+				fout.open("history.txt", ios::app);
+				fout << s << endl;
+				fout.close();
+			}
 		}
 		return s;
 	};
