a6d71b490d8aed5266e93daf795197ee9cb733e6
tvdtrong <tvdtrong@gmail.com>
2021-08-16 13:06:24 +0700

    fix show console function

--- project/utils/front_end.cpp
+++ project/utils/front_end.cpp
@@ -8,9 +8,7 @@
 void ShowConsoleCursor(bool showFlag)
 {
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
-
     CONSOLE_CURSOR_INFO     cursorInfo;
-
     GetConsoleCursorInfo(out, &cursorInfo);
     cursorInfo.bVisible = showFlag; // set the cursor visibility
     SetConsoleCursorInfo(out, &cursorInfo);
